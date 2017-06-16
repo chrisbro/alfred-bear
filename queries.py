165a7efc9604c3cfe9039d34786cce605a78b755
Chris Brown <chrisbro@gmail.com>
2017-06-16 08:38:23 -0400

    made the linter not hate my guts

--- queries.py
+++ queries.py
@@ -1,6 +1,17 @@
-import sqlite3
+#!/usr/bin/python
+# encoding: utf-8
 
-DB_LOCATION = ("/Library/Containers/net.shinyfrog.bear/Data/Library/Application Support/net.shinyfrog.bear/database.sqlite")
+"""
+Defines queries and execution functions for calling the Bear sqlite DB.
+"""
+
+import sqlite3
+import os
+import datetime
+
+DB_LOCATION = (
+    "/Library/Containers/net.shinyfrog.bear/Data/Library/Application Support/"
+    "net.shinyfrog.bear/database.sqlite")
 MAS_DB_LOCATION = DB_LOCATION.replace('.dunno', '.dunno.MacAppStore')
 DB_KEY = 'db_path'
 # todo: need to figure out what the actual app store install location is
@@ -17,63 +28,86 @@ NOTE_TITLE_SEARCH = (
 
 NOTE_TAG_SEARCH = (
     "SELECT "
-        "t.ZTITLE "
+    "t.ZTITLE "
     "FROM "
-        "ZSFNOTE n "
-        "INNER JOIN Z_5TAGS nt ON n.Z_PK = nt.Z_5NOTES "
-        "INNER JOIN ZSFNOTETAG t ON nt.Z_10TAGS = t.Z_PK "
+    "ZSFNOTE n "
+    "INNER JOIN Z_5TAGS nt ON n.Z_PK = nt.Z_5NOTES "
+    "INNER JOIN ZSFNOTETAG t ON nt.Z_10TAGS = t.Z_PK "
     "WHERE "
-        "n.ZARCHIVED=0 "
-        "AND n.ZTRASHED=0 "
-        "AND lower(t.ZTITLE) LIKE lower('%{0}%')")
+    "n.ZARCHIVED=0 "
+    "AND n.ZTRASHED=0 "
+    "AND lower(t.ZTITLE) LIKE lower('%{0}%')")
 
 
-def search_notes_by_title(wf, log, query):
-    sqlQuery = NOTE_TITLE_SEARCH.format(query)
-    #todo: search text as well as title, figure out best way to sort results
-    return run_query(wf, log, sqlQuery)
+def search_notes_by_title(workflow, log, query):
+    """
+    Searches for bear notes by the title of the note.
+    """
 
-def search_notes_by_tag(wf, log, query):
-    sqlQuery = NOTE_TAG_SEARCH.format(query)
-    return run_query(wf, log, sqlQuery)
+    sql_query = NOTE_TITLE_SEARCH.format(query)
+    return run_query(workflow, log, sql_query)
 
-def run_query(wf, log, sql):
-  db_path = wf.stored_data(DB_KEY)
-  if not db_path:
-    db_path = find_bear_db(log)
-    wf.store_data(DB_KEY, db_path)
-  else:
-    log.debug(db_path)
 
-  conn = sqlite3.connect(db_path)
-  cursor = conn.cursor()
-  log.debug(sql)
-  cursor.execute(sql)
-  results = cursor.fetchall()
-  log.debug("Found {0} results".format(len(results)))
-  cursor.close()
-  return results
+def search_notes_by_tag(workflow, log, query):
+    """
+    Searches for Bear notes by tag name.
+    """
+
+    sql_query = NOTE_TAG_SEARCH.format(query)
+    return run_query(workflow, log, sql_query)
+
+
+def run_query(workflow, log, sql):
+    """
+    Takes a SQL command, executes it, and returns the results.
+    """
+
+    db_path = workflow.stored_data(DB_KEY)
+    if not db_path:
+        db_path = find_bear_db(log)
+        workflow.store_data(DB_KEY, db_path)
+    else:
+        log.debug(db_path)
+
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    log.debug(sql)
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    log.debug("Found {0} results".format(len(results)))
+    cursor.close()
+    return results
 
 def find_bear_db(log):
-  home = os.path.expanduser("~")
-  db = "{0}{1}".format(home, DB_LOCATION)
-  mas = "{0}{1}".format(home, MAS_DB_LOCATION)
+    """
+    Finds the correct Bear sqlite3 DB - either a Mac app store installation,
+    or a normal installation. If both are present, it uses the one that was
+    last modified.
+    """
 
-  if not os.path.isfile(db):
-      log.debug("Bear db not found at {0}; using {1} instead".format(db, mas))
-      db = mas
-  elif os.path.isfile(mas):
-    db_mod = mod_date(db)
-    mas_mod = mod_date(mas)
-    if db_mod < mas_mod:
-        db = mas
-    log.debug("Bear direct and MAS db's found; using {0} as it's newer "
-              "(Direct {1} vs. MAS {2})".format(db, db_mod, mas_mod))
+    home = os.path.expanduser("~")
+    db_file = "{0}{1}".format(home, DB_LOCATION)
+    mas_db_file = "{0}{1}".format(home, MAS_DB_LOCATION)
 
-  log.debug(db)
-  return db
+    if not os.path.isfile(db_file):
+        log.debug(
+            "Bear db not found at {0}; using {1} instead".format(db_file, mas_db_file))
+        db_file = mas_db_file
+    elif os.path.isfile(mas_db_file):
+        db_mod = mod_date(db_file)
+        mas_mod = mod_date(mas_db_file)
+        if db_mod < mas_mod:
+            db_file = mas_db_file
+        log.debug("Bear direct and MAS db's found; using {0} as it's newer "
+                  "(Direct {1} vs. MAS {2})".format(db_file, db_mod, mas_mod))
 
+    log.debug(db_file)
+    return db_file
 
 def mod_date(filename):
-  mtime = os.path.getmtime(filename)
-  return datetime.datetime.fromtimestamp(mtime)
+    """
+    Returns the last modified date of a file.
+    """
+
+    mtime = os.path.getmtime(filename)
+    return datetime.datetime.fromtimestamp(mtime)
--- search.py
+++ search.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 # encoding: utf-8
 
-import os
+"""
+Main search script for alfred-bear workflow.
+"""
+
 import sys
 import argparse
-import datetime
 import queries
 from workflow import Workflow
 
-#TODOs
-# break SQL running out into separate file
 # create class for the note and populate/return that to main script? seems heavy
 # create note via create callback endpoint
 # append/prepend text to note via append callback endpoint
@@ -20,57 +20,73 @@ TAGS = "a"
 SINGLE_QUOTE = "'"
 ESC_SINGLE_QUOTE = "''"
 
-log = None
+LOGGER = None
 
-def main(wf):
-  log.debug('Started workflow')
-  args = parse_args()
 
-  if args.query:
-    query = args.query[0]
-    log.debug("Searching notes for '{0}'".format(query))
-    results = execute_search_query(args)
+def main(workflow):
+    """
+    I'm just here so I don't get fined by pylint
+    """
 
-  if not results:
-      wf.add_item('No search results found.')
-  else:
-    for result in results:
-      log.debug(result)
-      if args.type == TAGS:
-        wf.add_item(title=result[0], arg=result[0], valid=True)
-      else:
-        wf.add_item(title=result[1], arg=result[0], valid=True)
+    LOGGER.debug('Started workflow')
+    args = parse_args()
 
-  wf.send_feedback()
+    if args.query:
+        query = args.query[0]
+        LOGGER.debug("Searching notes for %s", format(query))
+        results = execute_search_query(args)
+
+    if not results:
+        workflow.add_item('No search results found.')
+    else:
+        for result in results:
+            LOGGER.debug(result)
+            if args.type == TAGS:
+                workflow.add_item(title=result[0], arg=result[0], valid=True)
+            else:
+                workflow.add_item(title=result[1], arg=result[0], valid=True)
+
+    workflow.send_feedback()
+
 
 def parse_args():
-  parser = argparse.ArgumentParser(description="Search Bear Notes")
-  parser.add_argument('-t', '--type', default=TITLE,
-    choices=[TITLE, TAGS],
-    type=str, help='What to search for: t(i)tle, or t(a)gs?')
-  parser.add_argument('query', type=unicode, nargs=argparse.REMAINDER, help='query string')
+    """
+    Parses out the arguments sent to the script in the Alfred workflow.
+    """
 
-  log.debug(wf.args)
-  args = parser.parse_args(wf.args)
-  return args
+    parser = argparse.ArgumentParser(description="Search Bear Notes")
+    parser.add_argument('-t', '--type', default=TITLE,
+                        choices=[TITLE, TAGS],
+                        type=str, help='What to search for: t(i)tle, or t(a)gs?')
+    parser.add_argument('query', type=unicode,
+                        nargs=argparse.REMAINDER, help='query string')
+
+    LOGGER.debug(WORKFLOW.args)
+    args = parser.parse_args(WORKFLOW.args)
+    return args
+
 
 def execute_search_query(args):
-  query = None
-  if args.query:
-    query = args.query[0]
+    """
+    Decides what search to run based on args that were passed in and executes the search.
+    """
+    query = None
+    if args.query:
+        query = args.query[0]
 
-    if SINGLE_QUOTE in query:
-        query = query.replace(SINGLE_QUOTE, ESC_SINGLE_QUOTE)
+        if SINGLE_QUOTE in query:
+            query = query.replace(SINGLE_QUOTE, ESC_SINGLE_QUOTE)
 
-  if args.type == TAGS:
-    log.debug('Searching tags')
-    results = queries.search_notes_by_tag(wf, log, query)
-  else:
-    log.debug('Searching tasks')
-    results = queries.search_notes_by_title(wf, log, query)
-  return results
+    if args.type == TAGS:
+        LOGGER.debug('Searching tags')
+        results = queries.search_notes_by_tag(WORKFLOW, LOGGER, query)
+    else:
+        LOGGER.debug('Searching tasks')
+        results = queries.search_notes_by_title(WORKFLOW, LOGGER, query)
+    return results
+
 
 if __name__ == '__main__':
-  wf = Workflow()
-  log = wf.logger
-  sys.exit(wf.run(main))
+    WORKFLOW = Workflow()
+    LOGGER = WORKFLOW.logger
+    sys.exit(WORKFLOW.run(main))
