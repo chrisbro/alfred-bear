fde1dbe9d50ced2379c71672cb0918d348f47176
Chris Redford <credford@gmail.com>
2018-01-15 18:00:03 -0800

    clipboard dependencies

---
+++ workflow/clipboard.py
@@ -0,0 +1,8 @@
+'''
+A croos platform clipboard read/write library.
+
+...well, as I were trying to implement this.
+I realize that everything is already in the pyperclip module.
+
+'''
+from pyperclip import copy, paste
